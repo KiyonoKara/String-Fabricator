33ebbb2309ef730c39022b1cafb6b15dd2393659
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-02-24 11:42:21 -0800

    Added more example(s) to the tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,3 +1,4 @@
 import { Japanese } from "../src";
 const jp = new Japanese();
 console.log(jp.extractJapaneseCharacters("おいしラメーン", "hiragana"));
+console.log(jp.getJapaneseCharacterCount("おはよう。。。まあ。。。実は6時だけど。。。"));
