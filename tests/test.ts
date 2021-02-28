eee0d3802d7a238bd2d9e9cb6ef826dc74ede60e
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-02-27 16:25:08 -0800

    Moved

--- tests/test.ts
+++
@@ -1,5 +0,0 @@
-import { Japanese } from "../src";
-const jp = new Japanese();
-console.log(jp.extractJapaneseCharacters("おいしラメーン", "hiragana"));
-console.log(jp.getJapaneseCharacterCount("おはよう。。。まあ。。。実は6時だけど。。。"));
-console.log(jp.fabricateJapaneseStrings(["《〭〭〭〭〭〭 》", "あいいいいいいい", "ＨＩＩＩＩＩＩ", "一丁丂七丄丅丆万丈三上下丌不与丏"]));
