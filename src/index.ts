831b978b49b92deaa81a7ea4623201f8a11d25e2
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-02-27 16:24:12 -0800

    Parse is now included

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Japanese } from "./Japanese/Japanese";
 export * from "./Capitalization/Capitalization";
 export * from "./CharacterRemoval/RemoveCharacters";
+export * from "./Parse/Parse";
