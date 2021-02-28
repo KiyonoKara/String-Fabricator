e56dbabfe6922334c74b002ab7fea02d10f77d62
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-02-27 16:24:45 -0800

    Added function called capitalizeLCOW which capitalizes the last letter of each word in a sentence

--- src/Capitalization/Capitalization.ts
+++ src/Capitalization/Capitalization.ts
@@ -1,5 +1,6 @@
 const capitalizeFirstChar = function(str): string { return str[0].toUpperCase() + str.slice(1); }
 const capitalizeFLEW = function(str): string { return str.replace(/\w\S*/g, strTxt => { return strTxt.charAt(0).toUpperCase() + strTxt.substr(1).toLowerCase(); }); }
 const mockingText = function(str): string { return str.toLowerCase().split("").map((i, j) => j % 2 === 0 ? i : i.toUpperCase()).join(""); }
+const capitalizeLCOW = function(str): string { return str.toLowerCase().replace(/[a-z]\b/g, letter => letter.toUpperCase()); }
 
-export { capitalizeFirstChar, mockingText, capitalizeFLEW };
+export { capitalizeFirstChar, mockingText, capitalizeFLEW, capitalizeLCOW };
