da8fa38913174e1e9e31475f4f8a0942ffc2fcc8
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-03-02 22:21:04 -0800

    Added more to the tests

--- tests/Japanese/test.ts
+++ tests/Japanese/test.ts
@@ -3,3 +3,4 @@ const jp = new Japanese();
 console.log(jp.extractJapaneseCharacters("おいしラメーン", "any"));
 console.log(jp.getJapaneseCharacterCount("おはよう。。。まあ。。。実は6時だけど。。。"));
 console.log(jp.fabricateJapaneseStrings(["《〭〭〭〭〭〭 》", "あいいいいいいい", "ＨＩＩＩＩＩＩ", "一丁丂七丄丅丆万丈三上下丌不与丏"], 'getJapaneseCharacterCount'));
+console.log(jp.cleanJapanese("いいひと. いいひと: いいひと! いいひと~いいひと, いいひと?"));
