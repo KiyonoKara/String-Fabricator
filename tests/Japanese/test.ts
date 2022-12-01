3c43ccb2e4cbe9d30b42d9dfa910875e98b7390f
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2022-12-01 23:27:56 +0000

    Rewrote tests on line 3, 4, 6

--- tests/Japanese/test.ts
+++ tests/Japanese/test.ts
@@ -1,6 +1,6 @@
 import { Japanese } from "../../src";
 const jp = new Japanese();
-console.log(jp.extractJapaneseCharacters("おいしラメーン", "any"));
-console.log(jp.getJapaneseCharacterCount("おはよう。。。まあ。。。実は6時だけど。。。"));
+console.log(jp.extractJapaneseCharacters("美味しいラメーン", "any"));
+console.log(jp.getJapaneseCharacterCount("おはようございます。。。実は、電話でございません"));
 console.log(jp.fabricateJapaneseStrings(["《〭〭〭〭〭〭 》", "あいいいいいいい", "ＨＩＩＩＩＩＩ", "一丁丂七丄丅丆万丈三上下丌不与丏"], 'getJapaneseCharacterCount'));
-console.log(jp.cleanJapanese("いいひと. いいひと: いいひと! いいひと~いいひと, いいひと?"));
+console.log(jp.cleanJapanese("いいひと. 良い人: 良いひと! 良い~良い, テスト?"));
